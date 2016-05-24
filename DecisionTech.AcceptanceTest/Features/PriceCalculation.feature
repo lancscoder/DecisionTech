4bb7f67979f0f63b2888fc1bf14d64f2490dbaf4
Simon Grimshaw <simongrimshaw@gmail.com>
2016-05-24 11:32:11 +0100

    Adding in test project and specflow.

---
+++ DecisionTech.AcceptanceTest/Features/PriceCalculation.feature
@@ -0,0 +1,29 @@
+﻿Feature: PriceCalculation
+	In order to calculate prices
+	As the checkout
+	I want to be told the sum of the basket
+
+Scenario: No discounts
+	Given the basket has 1 bread
+	And the basket has 1 butter
+	And the basket has 1 milk
+	When I total the basket
+	Then the total should be £2.95
+	
+Scenario: Bread discount
+	Given the basket has 2 butter
+	And the basket has 2 bread
+	When I total the basket
+	Then the total should be £3.10
+	
+Scenario: Milk discount
+	Given the basket has 4 milk
+	When I total the basket
+	Then the total should be £3.45
+	
+Scenario: Combined discount
+	Given the basket has 2 butter
+	And the basket has 1 bread
+	And the basket has 8 milk
+	When I total the basket
+	Then the total should be £9.00
---
+++ DecisionTech.AcceptanceTest/Steps/PriceCalculationSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace DecisionTech.AcceptanceTest.Steps
+{
+    [Binding]
+    public class PriceCalculationSteps
+    {
+        [Given(@"the basket has (.*) bread")]
+        public void GivenTheBasketHasBread(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the basket has (.*) butter")]
+        public void GivenTheBasketHasButter(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the basket has (.*) milk")]
+        public void GivenTheBasketHasMilk(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I total the basket")]
+        public void WhenITotalTheBasket()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the total should be £(.*)")]
+        public void ThenTheTotalShouldBe(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
