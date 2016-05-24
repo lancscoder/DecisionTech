172d274bb0258702731b9a514d2a94bde9b2fd70
Simon Grimshaw <simongrimshaw@gmail.com>
2016-05-24 21:30:10 +0100

    Refactoring bread discount code.

--- DecisionTech/Discounts/BreadDiscount.cs
+++ DecisionTech/Discounts/BreadDiscount.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DecisionTech.Products;
@@ -6,26 +7,27 @@ namespace DecisionTech.Discounts
 {
     public class BreadDiscount : IDiscount
     {
+        private readonly Func<BasketProduct, bool> _butterDiscountCondition = b => b.Product.GetType() == typeof(Butter) && !b.DiscountApplied;
+        private readonly Func<BasketProduct, bool> _breadDiscountCondition = b => b.Product.GetType() == typeof(Bread) && !b.DiscountApplied;
+
         public bool CanApplyDiscount(List<BasketProduct> basketContents)
         {
-            return basketContents.Count(b => b.Product.GetType() == typeof(Butter) && !b.DiscountApplied) >= 2 &&
-                   basketContents.Count(b => b.Product.GetType() == typeof(Bread) && !b.DiscountApplied) >= 1;
+            return basketContents.Count(_butterDiscountCondition) >= 2 &&
+                   basketContents.Count(_breadDiscountCondition) >= 1;
         }
 
         public void ApplyDiscount(List<BasketProduct> basketContents)
         {
             if (!CanApplyDiscount(basketContents)) return;
 
-            var butters =
-                basketContents.Where(b => b.Product.GetType() == typeof(Butter) && !b.DiscountApplied).Take(2);
+            var butters = basketContents.Where(_butterDiscountCondition).Take(2);
 
             foreach (var butter in butters)
             {
                 butter.DiscountApplied = true;
             }
 
-            var bread =
-                basketContents.FirstOrDefault(b => b.Product.GetType() == typeof(Bread) && !b.DiscountApplied);
+            var bread = basketContents.FirstOrDefault(_breadDiscountCondition);
 
             bread.BasketPrice = bread.BasketPrice * 0.5m;
             bread.DiscountApplied = true;
